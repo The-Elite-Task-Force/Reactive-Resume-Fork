d55c42386e88fcf6052bd3cd418253f1c7d44ed7
Mads Knudsen <madsfknudsen@hotmail.com>
2025-02-24 13:09:10 +0100

    endpoints done

--- apps/server/src/sectionItem/sectionItem.controller.ts
+++ apps/server/src/sectionItem/sectionItem.controller.ts
@@ -8,7 +8,7 @@ import {
   Param,
   Patch,
   Post,
-  UseGuards
+  UseGuards,
 } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { User as UserEntity } from "@prisma/client";
@@ -20,12 +20,10 @@ import { TwoFactorGuard } from "../auth/guards/two-factor.guard";
 import { User } from "../user/decorators/user.decorator";
 import { SectionItemService } from "./sectionItem.service";
 
-
 @ApiTags("SectionItem")
 @Controller("sectionItem")
 export class SectionItemController {
-  constructor(private readonly sectionItemService: SectionItemService) {
-  }
+  constructor(private readonly sectionItemService: SectionItemService) {}
 
   @Get("schema")
   getSchema() {
@@ -74,6 +72,149 @@ export class SectionItemController {
   async delete(@User() user: UserEntity, @Param("id") id: string) {
     try {
       return await this.sectionItemService.deleteSectionItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("work/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteWorkItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteWorkItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("award/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteAwardItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteAwardItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("skill/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteSkillItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteSkillItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("project/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteProjectItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteProjectItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("education/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteEducationItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteEducationItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("interest/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteInterestItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteInterestItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("language/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteLanguageItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteLanguageItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("volunteer/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteVolunteerItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteVolunteerItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("reference/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteReferenceItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteReferenceItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("publication/:id")
+  @UseGuards(TwoFactorGuard)
+  async deletePublicationItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deletePublicationItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("certification/:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteCertificationItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteCertificationItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("profile:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteProfileItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteProfileItem(id);
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Delete("basics:id")
+  @UseGuards(TwoFactorGuard)
+  async deleteBasicsItem(@User() user: UserEntity, @Param("id") id: string) {
+    try {
+      return await this.sectionItemService.deleteBasicsItem(id);
     } catch (error) {
       Logger.error(error);
       throw new InternalServerErrorException(error);
--- apps/server/src/sectionItem/sectionItem.service.ts
+++ apps/server/src/sectionItem/sectionItem.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, InternalServerErrorException, Logger } from "@nestjs/common";
+import { CreateSectionItemDto, UpdateSectionItemDto } from "@reactive-resume/dto";
 import { PrismaService } from "nestjs-prisma";
-import { CreateSectionItemDto, UpdateSectionItemDto, DeleteDto } from "@reactive-resume/dto";
 
 @Injectable()
 export class SectionItemService {
@@ -43,6 +43,123 @@ export class SectionItemService {
   async deleteSectionItem(id: string) {
     try {
       return await this.prisma.sectionItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteWorkItem(id: string) {
+    try {
+      return await this.prisma.workItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteAwardItem(id: string) {
+    try {
+      return await this.prisma.awardItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteSkillItem(id: string) {
+    try {
+      return await this.prisma.skillItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteProjectItem(id: string) {
+    try {
+      return await this.prisma.projectItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteEducationItem(id: string) {
+    try {
+      return await this.prisma.educationItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteInterestItem(id: string) {
+    try {
+      return await this.prisma.interestItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteLanguageItem(id: string) {
+    try {
+      return await this.prisma.languageItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteVolunteerItem(id: string) {
+    try {
+      return await this.prisma.volunteerItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteReferenceItem(id: string) {
+    try {
+      return await this.prisma.referenceItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deletePublicationItem(id: string) {
+    try {
+      return await this.prisma.publicationItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteCertificationItem(id: string) {
+    try {
+      return await this.prisma.certificationItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteProfileItem(id: string) {
+    try {
+      return await this.prisma.profileItem.delete({ where: { id } });
+    } catch (error) {
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteBasicsItem(id: string) {
+    try {
+      return await this.prisma.basicsItem.delete({ where: { id } });
     } catch (error) {
       Logger.error(error);
       throw new InternalServerErrorException(error);
