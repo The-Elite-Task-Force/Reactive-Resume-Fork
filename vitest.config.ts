7aaaf4190460ee3dda4b38f4a38f0c31267581fa
Mads Knudsen <madsfknudsen@hotmail.com>
2025-02-27 15:50:11 +0100

    added codecoverage html report

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // @ts-ignore
   test: {
     globals: true,
+    coverage: {
+      reporters: ["html"],
+      provider: "v8",
+      ignoreEmptyLines: true,
+      reportsDirectory: "./VitestCoverage",
+    },
     environment: "jsdom",
     alias: {
       // @ts-ignore
@@ -19,7 +26,11 @@ export default defineConfig({
       // @ts-ignore
       "@reactive-resume/schema": new URL("./libs/schema/src", import.meta.url).pathname,
       // @ts-ignore
-      "@/server/user/decorators/user.decorator": new URL("./apps/server/src/user/decorators/user.decorator", import.meta.url).pathname,
+      "@/server/user/decorators/user.decorator": new URL(
+        "./apps/server/src/user/decorators/user.decorator",
+        // @ts-ignore
+        import.meta.url,
+      ).pathname,
       // @ts-ignore
       "@reactive-resume/hooks": new URL("./libs/hooks/src", import.meta.url).pathname,
       // @ts-ignore
