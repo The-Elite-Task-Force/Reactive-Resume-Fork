1955c7c95b9ff7a218b87b398c2b0bd89c5c8d9c
Mads Knudsen <madsfknudsen@hotmail.com>
2025-02-27 11:03:34 +0100

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,29 @@
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  // @ts-ignore
   test: {
     alias: {
+      // @ts-ignore
+      "@/artboard/*": new URL("./apps/artboard/src*", import.meta.url).pathname,
+      // @ts-ignore
+      "@/client/*": new URL("./apps/client/src/*", import.meta.url).pathname,
+      // @ts-ignore
+      "@/server/*": new URL("./apps/server/src/*", import.meta.url).pathname,
+      // @ts-ignore
       "@reactive-resume/dto": new URL("./libs/dto/src", import.meta.url).pathname,
+      // @ts-ignore
       "@reactive-resume/utils": new URL("./libs/utils/src", import.meta.url).pathname,
+      // @ts-ignore
       "@reactive-resume/schema": new URL("./libs/schema/src", import.meta.url).pathname,
-      "@/server/user/decorators/user.decorator": new URL(
-        "./apps/server/src/user/decorators/user.decorator",
-        import.meta.url,
-      ).pathname,
+      // @ts-ignore
+      "@/server/user/decorators/user.decorator": new URL("./apps/server/src/user/decorators/user.decorator", import.meta.url).pathname,
+      // @ts-ignore
+      "@reactive-resume/hooks": new URL("./libs/hooks/src", import.meta.url).pathname,
+      // @ts-ignore
+      "@reactive-resume/parser": new URL("./libs/parser/src", import.meta.url).pathname,
+      // @ts-ignore
+      "@reactive-resume/ui": new URL("./libs/ui/src", import.meta.url).pathname,
     },
   },
 });
