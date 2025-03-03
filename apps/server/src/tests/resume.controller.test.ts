2769e788dfac52f00c284ef916b9fe3f0dfdb8bf
Mads Knudsen <madsfknudsen@hotmail.com>
2025-03-03 10:55:28 +0100

    updated based on comments from pull request

--- apps/server/src/tests/resume.controller.test.ts
+++ apps/server/src/tests/resume.controller.test.ts
@@ -1,6 +1,6 @@
-import * as process from "node:process";
+import process from "node:process";
 
-import { expect, vi } from "vitest";
+import { vi } from "vitest";
 
 import { ResumeController } from "../resume/resume.controller";
 import { mockUserWithoutPRI } from "./mocks/mocks";
@@ -14,10 +14,20 @@ describe("ResumeController", async () => {
   const { ResumeService } = await vi.importMock<typeof import("../resume/resume.service")>(
     "../resume/resume.service",
   );
+  // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+  const { SearchService } = await vi.importMock<typeof import("../search/search.service")>(
+    "../search/search.service",
+  );
+
   // @ts-expect-error We mock return values so we don't need to parse to the constructor
   const mockResumeService = new ResumeService();
+  // @ts-expect-error We mock return values so we don't need to parse to the constructor
+  const mockSearchService = new SearchService();
 
-  const controller: ResumeController = new ResumeController(mockResumeService);
+  const resumeController: ResumeController = new ResumeController(
+    mockResumeService,
+    mockSearchService,
+  );
   afterEach(() => {
     vi.clearAllMocks();
   });
@@ -27,13 +37,13 @@ describe("ResumeController", async () => {
       const user = mockUserWithoutPRI;
       const profileResumeId = "resumeId";
 
-      // @ts-expect-error The object returned gives error but still works.
       // eslint-disable-next-line @typescript-eslint/unbound-method
       vi.mocked(mockResumeService.setDefault).mockResolvedValue({
+        // @ts-expect-error The object returned gives error but still works.
         message: "Resume set as profile successfully",
       });
 
-      expect(await controller.setDefault(user, profileResumeId)).toEqual({
+      expect(await resumeController.setDefault(user, profileResumeId)).toEqual({
         message: "Resume set as profile successfully",
       });
     });
@@ -42,36 +52,24 @@ describe("ResumeController", async () => {
       const user = mockUserWithoutPRI;
       const profileResumeId = "resumeId";
 
-      const error = new Error("Failed to set resume as profile");
       // eslint-disable-next-line @typescript-eslint/unbound-method
-      vi.mocked(mockResumeService.setDefault).mockRejectedValue(error);
+      vi.mocked(mockResumeService.setDefault).mockRejectedValue({
+        message: "Failed to set resume as profile",
+      });
 
-      await expect(controller.setDefault(user, profileResumeId)).rejects.toThrow(
+      await expect(resumeController.setDefault(user, profileResumeId)).rejects.toThrow(
         "Failed to set resume as profile",
       );
     });
   });
 });
 
-describe("basic math test", () => {
-  afterEach(() => {
-    vi.clearAllMocks();
-  });
-
-  it("should return 2 when adding 1 + 1", () => {
-    expect(1 + 1).toBe(2);
-  });
-});
-
-describe(".env values", () => {
-  let postgresPort: number;
-  beforeEach(() => {
-    ///
-    /// .env file variables only work in test if they start with 'VITE_'
-    ///
-    // @ts-expect-error May be undefined.
-    postgresPort = +process.env.VITE_POSTGRES_PORT;
-  });
+describe(".env values example", () => {
+  ///
+  /// .env file variables only work in test if they start with 'VITE_'
+  ///
+  // @ts-expect-error May be undefined.
+  const postgresPort: number = +process.env.VITE_POSTGRES_PORT;
 
   it("should return postgres port", () => {
     expect(postgresPort).toBe(5432);
