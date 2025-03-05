0deeb1762a04e5080e90771b780552f38e294fcf
Kristofer Noda Pedersen <iluvmizuki@gmail.com>
2025-03-05 15:17:37 +0100

    moved all html to reutnr in company-logo.tsx

--- apps/client/src/pages/dashboard/company/company-logo.tsx
+++ apps/client/src/pages/dashboard/company/company-logo.tsx
@@ -4,33 +4,24 @@ import { getInitials } from "@reactive-resume/utils";
 type Props = {
   company: CompanyDto;
   size?: number;
-  className: string;
+  className?: string;
 };
 
 export const CompanyLogo = ({ company, size = 36, className }: Props) => {
-  let picture: React.ReactNode;
+  const style = { width: size, height: size };
 
-  if (company.picture) {
-    picture = (
-      <img
-        alt={company.name}
-        src={company.picture}
-        className="rounded-full"
-        style={{ width: size, height: size }}
-      />
-    );
-  } else {
-    const initials = getInitials(company.name);
-
-    picture = (
-      <div
-        style={{ width: size, height: size }}
-        className="flex items-center justify-center rounded-full bg-secondary text-center text-[10px] font-semibold text-secondary-foreground"
-      >
-        {initials}
-      </div>
-    );
-  }
-
-  return <div className={className}>{picture}</div>;
+  return (
+    <div className={className}>
+      {company.picture ? (
+        <img alt={company.name} src={company.picture} className="rounded-full" style={style} />
+      ) : (
+        <div
+          style={style}
+          className="flex items-center justify-center rounded-full bg-secondary text-center text-[10px] font-semibold text-secondary-foreground"
+        >
+          {getInitials(company.name)}
+        </div>
+      )}
+    </div>
+  );
 };
