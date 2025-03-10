c4eabcb38ab261704a67d4deec6ab3deffa7be6b
Kristofer Noda Pedersen <22773107+KurisuDaNoda@users.noreply.github.com>
2025-03-10 12:34:23 +0100

    Update translate-error.ts

--- apps/client/src/services/errors/translate-error.ts
+++ apps/client/src/services/errors/translate-error.ts
@@ -42,10 +42,6 @@ export const translateError = (error: ERROR_MESSAGE) => {
     case ERROR_MESSAGE.ResumeSlugAlreadyExists: {
       return t`A resume with this alias already exists, please pick a different unique identifier.`;
     }
-    // case ERROR_MESSAGE.ResumeSlugAlreadyExists: {
-    //   return t`A resume with this slug already exists, please pick a different unique identifier.`;
-    // }
-    // // This is the old slug error in case it needs to be reverted
     case ERROR_MESSAGE.ResumeNotFound: {
       return t`It looks like the resume you're looking for doesn't exist.`;
     }
